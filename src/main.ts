7b0478952fa5563cc4c84e1439be0ac12354d3be
ayophanz <ayophanz18@gmail.com>
2024-04-03 11:33:18 +0800

    setup micro-frontend routing

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,18 @@
-import { registerApplication, start } from 'single-spa';
-import {
-	constructApplications,
-	constructRoutes,
-	constructLayoutEngine
-} from 'single-spa-layout';
-import '@loongwoo/tailwind';
+import { registerApplication, start } from "single-spa";
+import * as isActive from "../src/activity-function";
 
-import microfrontendLayout from './microfrontend-layout.html?raw';
+registerApplication(
+  "@invoicin/nav-ui",
+  () => System.import("@invoicin/nav-ui"),
+  isActive.nav,
+  { domElement: document.getElementById('nav-container') }
+);
 
-const routes = constructRoutes(microfrontendLayout);
-const applications = constructApplications({
-	routes,
-	loadApp: ({ name }) => import(/* @vite-ignore */ name)
-});
-const layoutEngine = constructLayoutEngine({ routes, applications });
+registerApplication(
+  "@invoicin/organization-ui",
+  () => System.import("@invoicin/organization-ui"),
+  isActive.organization,
+  { domElement: document.getElementById('organization-container') }
+);
 
-applications.forEach(registerApplication);
-layoutEngine.activate();
 start();
